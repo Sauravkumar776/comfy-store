14bde2282d1c2aab8053ad4cfa08d9aac55a1102
Sauravkumar776 <ashukumar752861@gmail.com>
2023-09-29 21:27:30 +0530

    Adds Feature Products.

---
+++ comfy-store/src/components/FeaturedProducts.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SectionTitle from './SectionTitle';
+import ProductsGrid from './ProductsGrid';
+
+type Props = {}
+
+const FeaturedProducts = (props: Props) => {
+  return (
+    <div className='pt-24 '>
+    <SectionTitle text='featured products' />
+    <ProductsGrid />
+  </div>
+  )
+}
+
+export default FeaturedProducts
