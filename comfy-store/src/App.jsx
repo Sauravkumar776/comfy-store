93eb00c8418556e03028c08327823eddc452fdde
Sauravkumar776 <ashukumar752861@gmail.com>
2023-12-17 19:58:06 +0530

    Adds more contents for the website.

--- comfy-store/src/App.jsx
+++ comfy-store/src/App.jsx
@@ -14,6 +14,8 @@ import {
 } from "./pages";
 import { ErrorElement } from "./components";
 import { loader as landingLoader } from './pages/Landing';
+import { loader as singleProductLoader } from './pages/SingleProduct';
+import { loader as productsLoader } from './pages/Products';
 
 const router = createBrowserRouter([
   {
@@ -30,10 +32,14 @@ const router = createBrowserRouter([
       {
         path: "products",
         element: <Products />,
+        loader: productsLoader,
+        errorElement: <ErrorElement />,
       },
       {
         path: "products/:id",
         element: <SingleProduct />,
+        loader: singleProductLoader,
+        errorElement: <ErrorElement />,
       },
       {
         path: "cart",
